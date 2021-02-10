aa8b1f267b57a80241bfbcb7d884e4629c5a6f33
Eddy Boughioul <eddyboughioul@hotmail.fr>
2021-02-10 10:20:06 +0100

    Add notifypropertychanged

--- MVVM/ViewModels/ContactViewModel.cs
+++ MVVM/ViewModels/ContactViewModel.cs
@@ -3,6 +3,7 @@ using MVVM.Tools;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Configuration;
 using System.Text;
 using System.Windows;
@@ -13,18 +14,20 @@ namespace MVVM.ViewModels
     /// <summary>
     /// Notre ViewModel
     /// </summary>
-    public class ContactViewModel
+    public class ContactViewModel : BaseViewModel
     {
         /// <summary>
         /// L'observable collection est une liste qui notifie de ses changements en cas d'ajout et de suppression d'item
         /// </summary>
         public ObservableCollection<ContactModel> Contacts { get; set; }
 
+        private ContactModel contactSelected;
 
         public ContactModel ContactSelected
         {
-            get;
-            set;
+            get { return contactSelected; }
+            // set property se trouve dans BaseViewModel et permet de notifier d'un changement de valeur, elle se base sur l'interface INotifyPropertyChanged
+            set { SetProperty(ref contactSelected, value); }
         }
 
 
@@ -46,8 +49,22 @@ namespace MVVM.ViewModels
                 FirstName = "Toto",
                 LastName = "Ducobu"
             };
+            var item2 = new ContactModel()
+            {
+                BirthYear = 1986,
+                FirstName = "Tata",
+                LastName = "Yoyo"
+            };
+            var item3 = new ContactModel()
+            {
+                BirthYear = 1990,
+                FirstName = "Lolo",
+                LastName = "Williams"
+            };
             // on ajoute au moin un item pour l'afficher au démarrage
             Contacts.Add(item);
+            Contacts.Add(item2);
+            Contacts.Add(item3);
             AddCommand = new RelayCommand<ContactModel>((e) => AddContact());
             EditCommand = new RelayCommand<ContactModel>((c) => EditContact(c));
             DeleteCommand = new RelayCommand<ContactModel>((c) => DeleteContact(c));
